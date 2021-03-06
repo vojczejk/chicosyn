9fc334c6a4cb1a417e1592bbfac7d77ebce3b515
Vojtěch Nevřela <nevrelav@gmail.com>
2021-03-06 01:14:34 +0100

    bad lut value, moved lut to c file, key parsing

--- include/ps2.h
+++ include/ps2.h
@@ -3,7 +3,14 @@
 
 #include <avr/interrupt.h>
 
+#include "lut.h"
+
 void ps2_init(void);
+
+
+//keys_t ps2_scan_to_keyboard(uint8_t scan_byte);
+
+
 
 
 #endif 
--- src/ps2.c
+++ src/ps2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "ps2.h"
+#include "oscillator.h"
 
 void ps2_init(void)
 {
@@ -10,6 +11,85 @@ void ps2_init(void)
     EIFR  |=(1<<INTF1);
 }
 
+keys_t ps2_scan_to_keyboard(uint8_t scan_byte)
+{
+    switch (scan_byte)
+    {
+        case 0x1A:
+            return C0;
+        case 0x1B:
+            return C0s;
+        case 0x22:
+            return D0;
+        case 0x23:
+            return D0s;
+        case 0x21:
+            return E0;
+        case 0x2A:
+            return F0;
+        case 0x34:
+            return F0s;
+        case 0x32:
+            return G0;
+        case 0x33:
+            return G0s;
+        case 0x31:
+            return A0;
+        case 0x3B:
+            return A0s;
+        case 0x3A:
+            return B0;
+        case 0x41:
+        case 0x15:
+            return C1;
+        case 0x4B:
+        case 0x1E:
+            return C1s;
+        case 0x49:
+        case 0x1D:
+            return D1;
+        case 0x4C:
+        case 0x26:
+            return D1s;
+        case 0x4A:
+        case 0x24:
+            return E1;
+        case 0x2D:
+            return F1;
+        case 0x2E:
+            return F1s;
+        case 0x2C:
+            return G1;
+        case 0x36:
+            return G1s;
+        case 0x35:
+            return A1;
+        case 0x3D:
+            return A1s;
+        case 0x3C:
+            return B1;
+        case 0x43:
+            return C2;
+        case 0x46:
+            return C2s;
+        case 0x44:
+            return D2;
+        case 0x45:
+            return D2s;
+        case 0x4D:
+            return E2;
+        case 0x54:
+            return F2;
+        case 0x55:
+            return F2s;
+        case 0x5B:
+            return G2;
+        default:
+            return KEY_CNT;
+    }
+    return KEY_CNT;
+}
+ 
 ISR(INT1_vect)
 {
     static unsigned char data;// Holds the received scan code
@@ -33,7 +113,13 @@ ISR(INT1_vect)
     {   
         if(--bitcount == 0)// All bits received
         {
+            uint8_t tmp;
             printf("%x\n\r",data);
+            tmp = ps2_scan_to_keyboard(data);
+            if(tmp != KEY_CNT)
+            {
+		        g_main_osc.note = (uint8_t)tmp + 60;
+            }
             bitcount = 11;
             data = 0;
         }
